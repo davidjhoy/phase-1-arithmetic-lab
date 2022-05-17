4c4559e2071a49a266d187e358af247090cc4369
david <davidjhoyjr@gmail.com>
2022-05-17 09:56:03 -0400

    commit tree 36ff16bca5fb0219fe8b61fa676ce352d53c3326

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const num3 = 14;
+const num4 = 5;
+
+
+let multiply;
+multiply = num1*num2;
+
+
+let random;
+random = Math.floor(Math.random()*10) + 1 ;
+
+
+let mod;
+mod = num3%num4;
+
+
+let max = Math.max(2, 20, 4, 5, 3);
+
+
+
